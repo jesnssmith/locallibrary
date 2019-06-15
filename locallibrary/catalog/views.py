dc120d37fab80fec9b2cb3453ac6635fc0be55b1
jesns smith <smithjesns@gmail.com>
2019-06-15 23:39:18 +0800

    'step 8'

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -41,5 +41,27 @@ class AuthorListView(generic.ListView):
     model = Author
 
 
+from datetime import date
+
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        today = date.today
+        context['today'] = today
+        return context
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user. 
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
